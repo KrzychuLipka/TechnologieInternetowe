c819a267fb0d49684c52f92032882a6e4d31e9e6
Krzysztof Lipka <klipka1991@gmail.com>
2026-04-03 13:04:31 +0200

    New box loading mechanism

--- composeApp/src/commonMain/kotlin/pl/lipov/technologieinternetowe/presentation/gameList/BoxImage.kt
+++ composeApp/src/commonMain/kotlin/pl/lipov/technologieinternetowe/presentation/gameList/BoxImage.kt
@@ -2,10 +2,17 @@ package pl.lipov.technologieinternetowe.presentation.gameList
 
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import com.seiko.imageloader.rememberImagePainter
+import org.jetbrains.compose.resources.painterResource
 import pl.lipov.technologieinternetowe.domain.model.Game
+import technologieinternetowe.composeapp.generated.resources.Res
+import technologieinternetowe.composeapp.generated.resources.retro9
 
 @Composable
 fun BoxImage(
@@ -13,8 +20,24 @@ fun BoxImage(
     game: Game,
     contentScale: ContentScale = ContentScale.FillHeight
 ) {
-    val imageUrl = "TODO/${game.id}.png"
-    val painter = rememberImagePainter(imageUrl)
+    val lowerCaseUrl =
+        "https://raw.githubusercontent.com/KrzychuLipka/TechnologieInternetowe/final/boxes/${game.id}.png"
+    val upperCaseUrl =
+        "https://raw.githubusercontent.com/KrzychuLipka/TechnologieInternetowe/final/boxes/${game.id}.PNG"
+
+    var useUpperCase by remember { mutableStateOf(false) }
+
+    val painter = rememberImagePainter(
+        url = if (useUpperCase) upperCaseUrl else lowerCaseUrl,
+        errorPainter = {
+            if (!useUpperCase) {
+                useUpperCase = true
+            }
+
+            painterResource(Res.drawable.retro9)
+        }
+    )
+
     Image(
         modifier = modifier,
         painter = painter,
